4de98a84c8c2554622a35e00df2ba85325e7cf10
Jerry Liu <ljliu89@gmail.com>
2025-09-01 23:37:59 -0700

    Add table

---
+++ src/components/Table.tsx
@@ -0,0 +1,21 @@
+const Table = ({
+  columns,
+}: {
+  columns: { header: string; accessor: string; className?: string }[];
+}) => {
+  return (
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-left text-gray-500 text-sm">
+          {columns.map((col) => (
+            <th key={col.accessor} className={col.className}>
+              {col.header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+    </table>
+  );
+};
+
+export default Table;
